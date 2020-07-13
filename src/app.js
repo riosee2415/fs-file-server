902919f0ec932faddcad3f89a591cdbb46e10efa
4leaf.ysh <4leaf.ysh@gmail.com>
2020-07-13 22:31:20 +0900

    first commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.post("/api/test", (req, res) => {
+  console.log("Server Is Called By Client");
+  console.log(req.body.params.inputData);
+
+  const {
+    body: {
+      params: { inputData },
+    },
+  } = req;
+
+  console.log(inputData);
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ ${PORT} SERVER START!`);
+});
